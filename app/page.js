5ff0ca614e2ed90a110e686b1838235d9b153b0c
starbvuks <starbvuks@gmail.com>
2024-05-15 21:08:29 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,118 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
+import NavBar from "../components/Navbar";
+import { Button, Link } from "@nextui-org/react";
+
+import ProgressSlider from "@/components/Carousel";
+
+const data = {
+  cards: [
+    {
+      image: "/cards/nlp.png",
+      header: "NLP-Based Discovery Engine",
+      content:
+        "Input natural language queries to find professionals and opportunities that align perfectly with your needs. Our AI-driven search provides precise and relevant results, enhancing your networking experience.",
+    },
+    {
+      image: "/cards/ai.png",
+      header: "AI-Powered Assistant",
+      content:
+        "Input natural language queries to find professionals and opportunities that align perfectly with your needs. Our AI-driven search provides precise and relevant results, enhancing your networking experience.",
+    },
+    {
+      image: "/cards/network.png",
+      header: "Networking and Collaboration",
+      content:
+        "Build meaningful professional relationships through our sophisticated networking features. Share insights, collaborate on projects, and connect with like-minded professionals effortlessly.",
+    },
+  ],
+};
 
 export default function Home() {
+  const items = [
+    {
+      img: "tm-logo.png",
+      desc: "talentmapp",
+      buttonIcon: "/tm-logo.png",
+      content: "Aliqua cupidatat id duis irure sunt exercitation voluptate cillum. Consectetur ad ex do in reprehenderit est dolor elit et exercitation do ad. Voluptate excepteur magna ea ad aliquip ad."
+    },
+    {
+      img: "/logos/gdn-logo.png",
+      desc: "Go Digital Now",
+      buttonIcon: "/logos/gdn-logo.png",
+      content: "Aliqua cupidatat id duis irure sunt exercitation voluptate cillum. Consectetur ad ex do in reprehenderit est dolor elit et exercitation do ad. Voluptate excepteur magna ea ad aliquip ad."
+
+    },
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center bg-white font-jakarta pb-24">
+      <div className="bg-[#5013AF] h-12 w-full flex items-center justify-center">
+        <span className="text-[#DDD1F0] font-jakarta font-extralight">
+          For product announcements and exclusive insights.{" "}
+          <span className="text-white">Subscribe Now</span>
+        </span>
+        <img src="banner-overlay.svg" alt="overlay" className="absolute" />
+      </div>
+      <NavBar />
+      {/* SECTION 1 */}
+      <div className="flex items-center justify-between w-full px-16">
+        <div className="w-[50%] py-24  flex flex-col text-6xl font-bold tracking-tight">
+          <span className=" ">Discover the Right</span>
+          <span className="text-[#5013AF]">Connections</span>
+          <span>Faster Than Ever</span>
+          <span className="font-light text-lg mt-3 w-4/5 tracking-normal">
+            Harness the power of AI to refine and expedite your professional
+            networking.
+          </span>
+          <Button
+            as={Link}
+            href="#"
+            size="lg"
+            radius="sm"
+            className="bg-[#5013AF] w-1/3 mt-9 tracking-normal text-white"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Start Your Search
+          </Button>
+        </div>
+        <div className="w-[50%]">
+          <img src="hero-image.png" alt="hero" className="self-end" />
         </div>
       </div>
+      {/* SECTION 2 */}
+      <div className="w-11/12 flex flex-col justify-center items-center text-center mt-52">
+        <span className="text-4xl font-semibold w-1/2">
+          Take the next step & get results faster
+        </span>
+        <div className="flex gap-14 text-left justify-between mt-16 w-full">
+          {data.cards.map((card, index) => (
+            <div
+              key={index}
+              className="flex flex-col justify-between bg-white shadow-2xl shadow-[#9986b6] w-1/3 p-5 rounded-xl"
+            >
+              <img
+                src={card.image}
+                alt={card.header}
+                className="w-1/4 h-auto"
+              />
+              <span className="text-4xl font-semibold">{card.header}</span>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+              <span className="text-lg font-light mt-2">{card.content}</span>
+              <Button
+                as={Link}
+                href="#"
+                size="lg"
+                radius="sm"
+                className="bg-[#5013AF] py-6 mt-3 tracking-normal text-white"
+              >
+                Learn More
+              </Button>
+            </div>
+          ))}
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="mt-32 w-full ">
+          <ProgressSlider items={items} />
+        </div>
     </main>
   );
 }
